80e23b2c3449afbe1be8e1880c1bbea25dee3650
Lukas <69743171+quick007@users.noreply.github.com>
2024-10-28 16:52:15 -0700

    crystal balls problem

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,6 +1,7 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     let low = 0;
     let high = haystack.length;
+
     do {
         const mid = Math.floor(low + (high - low) / 2);
         const value = haystack[mid];
@@ -10,7 +11,7 @@ export default function bs_list(haystack: number[], needle: number): boolean {
         }
 
         if (value > needle) {
-           high = mid;
+            high = mid;
         } else {
             low = mid + 1;
         }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jump = Math.floor(Math.sqrt(breaks.length));
+    let prev = 0;
 
+    for (let i = 0; i < breaks.length; i += jump) {
+        if (breaks[i] === true) {
+            for (let j = prev; j <= i; j++) {
+                if (breaks[j]) {
+                    return j;
+                }
+            }
+        }
+        prev = i;
+    }
+    return -1;
 }
