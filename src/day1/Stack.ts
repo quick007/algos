34a2c589193298686a0d7044b506d48850062b18
Lukas <69743171+quick007@users.noreply.github.com>
2024-10-29 16:26:53 -0700

    stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private stack?: Node<T>;
 
     constructor() {
+        this.length = 0;
     }
 
     push(item: T): void {
+        this.stack = {
+            value: item,
+            prev: this.stack,
+        };
+        this.length++;
+    }
 
-}
     pop(): T | undefined {
+        const temp = this.stack;
 
-}
+        if (!temp) {
+            return undefined;
+        }
+
+        this.stack = temp.prev;
+        this.length--;
+        return temp.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.stack?.value;
+    }
 }
