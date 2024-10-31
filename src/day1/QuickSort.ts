a27d4fafabba2a39d109e676a4358a60c6225f7e
Lukas <69743171+quick007@users.noreply.github.com>
2024-10-30 18:20:16 -0700

    quick007 sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,36 @@
-export default function quick_sort(arr: number[]): void {
+// produce pivot index + move stuff around
+function partition(arr: number[], low: number, high: number): number {
+	const pivot = arr[high]
+	let index = low - 1
 
+	for (let i = low; i < high; i++) {
+		if (arr[i] <= pivot) {
+			index++
+			const temp = arr[i]
+			arr[i] = arr[index]
+			arr[index] = temp
+		}
+	}
+
+	index++
+	arr[high] = arr[index]
+	arr[index] = pivot
+
+	return index
+}
+
+// call partition, bookkeepting, recusrion, etc.
+function qs(arr: number[], low: number, high: number): void {
+	if (low >= high) {
+		return;
+	}
+
+	const pivotIndex = partition(arr, low, high)
+
+	qs(arr, low, pivotIndex - 1)
+	qs(arr, pivotIndex + 1, high)
+}
+
+export default function quick_sort(arr: number[]): void {
+	qs(arr, 0, arr.length - 1)
 }
