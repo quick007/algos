5b0044db1f9f858c1f46dbd3922b243f8f474e62
Lukas <69743171+quick007@users.noreply.github.com>
2024-11-01 22:35:11 -0700

    start making a trie

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,18 +1,66 @@
-export default class Trie {
-    
+interface Node {
+    value: string;
+    children: (Node | null)[];
+    isWord: boolean;
+}
 
-    
+export default class Trie {
+    // public length: number;
+    private head: Node;
 
     constructor() {
+        // this.length = 0;
+        this.head = {
+            value: "",
+            children: this.newArray(),
+            isWord: false,
+        };
     }
 
     insert(item: string): void {
+        this.newWord(item, this.head)
+    }
 
-}
     delete(item: string): void {
 
-}
+    }
+
     find(partial: string): string[] {
 
-}
+    }
+
+    private newWord(item: string, node: Node) {
+        if (item.length === 0) {
+            return;
+        }
+
+        const letter = item.charAt(0);
+        const pos = this.getPos(letter)
+        item = item.slice(1)
+
+        if (!node.children[pos]) {
+            node.children[pos] = {
+                value: letter,
+                children: this.newArray(),
+                isWord: false
+            }
+        }
+
+        if (item.length === 0) {
+            node.children[pos] = {
+                ...node.children[pos],
+                isWord: true
+            }
+        }
+
+        this.newWord(item, node.children[pos])
+    }
+
+    private newArray() {
+        return Array(26).fill(null)
+    }
+
+    private getPos(char: string) {
+        return char.charCodeAt(0) - "a".charCodeAt(0)
+    }
 }
