87fcdbe3f7c87f8a6fbc98d652849a7b0285539f
Lukas <69743171+quick007@users.noreply.github.com>
2024-11-02 20:10:39 -0700

    BFS on a graph matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,54 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+import Queue from "./Queue";
 
+export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+	const q = new Queue<number>();
+	const seen: boolean[] = new Array(graph.length).fill(false);
+	const prev: number[] = new Array(graph.length).fill(-1)
+
+	q.enqueue(source);
+	seen[source] = true;
+	
+	while (q.length) {
+		const curr = q.deque()
+
+		if (curr === needle) {
+			break;
+		}
+
+		if (curr === undefined) {
+			break;
+		}
+
+		const connected = graph[curr];
+
+		for (let i = 0; i < connected.length; i++) {
+			const v = connected[i]
+			if (seen[i] || v === 0) { // no connection or has been seen before
+				continue;
+			}
+			seen[i] = true
+			prev[i] = curr
+			q.enqueue(i)
+		}
+	}
+
+	if (prev[needle] === -1) {
+		return null
+	}
+
+	const path: number[] = []
+	let v = needle
+
+	while (v !== -1) {
+		path.push(v)
+		v = prev[v]
+	}
+
+	// for (let i = 0; i < Math.floor(path.length / 2); i++) {
+	// 	const temp = path[i]
+	// 	path[i] = path[path.length - i];
+	// 	path[path.length - i] = temp;
+	// }
+
+	return path.reverse()
 }
--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -18,16 +18,34 @@ export default class Trie {
     }
 
     insert(item: string): void {
-        this.newWord(item, this.head)
+        this.newWord(item, this.head);
     }
 
     delete(item: string): void {
+        let node = this.head;
+        let prevIsWord: Node | undefined = undefined
+        for (let i = 0; i < item.length; i++) {
+            const letter = item.charAt(i);
+            const pos = this.getPos(letter);
+            const thisNode = node.children[pos];
 
+            if (!thisNode) {
+                return;
+            }
+
+            if (thisNode.isWord) {
+                prevIsWord = thisNode
+            }
+
+            node = thisNode;
+        }
+
+        if (prevIsWord) {
+
+        }
     }
 
-    find(partial: string): string[] {
-
-    }
+    find(partial: string): string[] {}
 
     private newWord(item: string, node: Node) {
         if (item.length === 0) {
@@ -35,32 +53,32 @@ export default class Trie {
         }
 
         const letter = item.charAt(0);
-        const pos = this.getPos(letter)
-        item = item.slice(1)
+        const pos = this.getPos(letter);
+        item = item.slice(1);
 
         if (!node.children[pos]) {
             node.children[pos] = {
                 value: letter,
                 children: this.newArray(),
-                isWord: false
-            }
+                isWord: false,
+            };
         }
 
         if (item.length === 0) {
             node.children[pos] = {
                 ...node.children[pos],
-                isWord: true
-            }
+                isWord: true,
+            };
         }
 
-        this.newWord(item, node.children[pos])
+        this.newWord(item, node.children[pos]);
     }
 
     private newArray() {
-        return Array(26).fill(null)
+        return Array(26).fill(null);
     }
 
     private getPos(char: string) {
-        return char.charCodeAt(0) - "a".charCodeAt(0)
+        return char.charCodeAt(0) - "a".charCodeAt(0);
     }
 }
