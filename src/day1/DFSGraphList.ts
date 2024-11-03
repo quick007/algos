1861ae60ebeec5a2b825d7cf3071b511aa082aae
Lukas <69743171+quick007@users.noreply.github.com>
2024-11-03 14:23:17 -0800

    DFS graph list

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,48 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, node: number, needle: number, path: number[], seen: boolean[]): boolean {
+	if (seen[node]) {
+		return false;
+	}
 
+	if (node === needle) {
+		path.push(node)
+		return true;
+	}
+
+	//pre
+	seen[node] = true
+	path.push(node)
+
+	//recurse
+	const connections = graph[node]
+	for (let i = 0; i < connections.length; i++) {
+		const c = connections[i]
+		if (
+			walk(
+				graph, 
+				c.to,
+				needle,
+				path,
+				seen
+			)
+		) {
+			return true
+		}
+	}
+
+	//post
+	path.pop()
+	return false
+
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+	const seen: boolean[] = new Array(graph.length).fill(false);
+	const path: number[] = [];
+
+	walk(graph, source, needle, path, seen)
+
+	if (path.length) {
+		return path
+	}
+	return null
 }
--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,7 +1,7 @@
 interface Node {
     value: string;
     children: (Node | null)[];
-    isWord: boolean;
+    isWord: boolean; // I really should have done this with numbers, but oh well
 }
 
 export default class Trie {
@@ -23,7 +23,7 @@ export default class Trie {
 
     delete(item: string): void {
         let node = this.head;
-        let prevIsWord: Node | undefined = undefined
+        let prevIsWord: {node: Node, next: string | undefined} | undefined = undefined
         for (let i = 0; i < item.length; i++) {
             const letter = item.charAt(i);
             const pos = this.getPos(letter);
@@ -34,18 +34,40 @@ export default class Trie {
             }
 
             if (thisNode.isWord) {
-                prevIsWord = thisNode
+                const letter2 = item.charAt(i + 1);
+                const pos2 = this.getPos(letter);
+                prevIsWord = {
+                    node: thisNode,
+                    next: thisNode.children[pos2]?.value
+                }
             }
 
             node = thisNode;
         }
 
-        if (prevIsWord) {
-
+        if (prevIsWord && prevIsWord.next) {
+            prevIsWord.node.children[this.getPos(prevIsWord.next)] = null;
         }
     }
 
-    find(partial: string): string[] {}
+    find(partial: string): string[] {
+        const words: string[] = []
+
+        let node: Node = this.head
+        for (let i = 0; i < partial.length; i++) {
+            const pos = this.getPos(partial[i])
+
+            if (node.children[pos] === null) {
+                return words
+            }
+
+            node = node.children[pos]!
+        }
+
+        this.findWords(node, partial, words)
+
+        return words
+    }
 
     private newWord(item: string, node: Node) {
         if (item.length === 0) {
@@ -66,12 +88,25 @@ export default class Trie {
 
         if (item.length === 0) {
             node.children[pos] = {
-                ...node.children[pos],
+                ...node.children[pos]!,
                 isWord: true,
             };
         }
 
-        this.newWord(item, node.children[pos]);
+        this.newWord(item, node.children[pos]!);
+    }
+
+    private findWords(node: Node, prefix: string, words: string[]) {
+        if (node.isWord) {
+            words.push(prefix)
+        }
+
+        for (let i = 0; i < node.children.length - 1; i++) {
+            if (node.children[i] !== null) {
+                const char = String.fromCharCode(i + 'a'.charCodeAt(0))
+                this.findWords(node.children[i]!, prefix + char, words)
+            }
+        }
     }
 
     private newArray() {
